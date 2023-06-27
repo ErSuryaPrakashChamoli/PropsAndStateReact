6cc21e7f0d93f8d0c06ba376137ec0ec8d75ae87
suryaprakashchamoli <suryaprakashchamoli@gmail.com>
2023-06-27 23:03:00 +0530

    nehal props and state

---
+++ src/Nehal.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Sooraj from './Sooraj'
+
+export default function Nehal() {
+   
+    const name = 'ashu';
+    const age = '27';
+
+  return (
+    <div>
+
+<p>
+    Hi, I'm a software engineer.
+</p>
+
+
+
+<Sooraj   name = {name} age ={age}  />
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
---
+++ src/Sooraj.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Sooraj(props) {
+  return (
+    <div>
+        Sooraj is big than {props.name} and his age is {props.age}
+        </div>
+  )
+}
